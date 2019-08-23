29619f82fc9d4e2d3bf41e4634309df14f221c06
Efe <m.efetiryaki@gmail.com>
2019-08-23 11:42:07 +0200

    travis

---
+++ test/node_test_trial.cpp
@@ -0,0 +1,22 @@
+
+
+#include <ros/ros.h>
+#include <vector>
+#include <Eigen/Dense>
+#include <gtest/gtest.h>
+
+using namespace gazebo;
+using namespace wrench;
+
+TEST(TestSuite, testCase1)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+
+}
